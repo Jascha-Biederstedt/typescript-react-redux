4d1912324591dfc2dfbb8533ba97cca257bc19b7
Jascha-Biederstedt <j.biederstedt@gmx.de>
2022-06-09 09:35:30 +0200

    Implement recorder functionality

--- src/components/Recorder/Recorder.tsx
+++ src/components/Recorder/Recorder.tsx
@@ -1,26 +1,56 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { selectDateStart, start } from '../../redux/recorder';
+import { selectDateStart, start, stop } from '../../redux/recorder';
 
 import styles from './Recorder.module.css';
+
+const addZero = (num: number) => (num < 10 ? `0${num}` : `${num}`);
 
 const Recorder = () => {
   const dispatch = useDispatch();
   const dateStart = useSelector(selectDateStart);
+  const [, setCount] = useState<number>(0);
 
   const started = dateStart !== '';
+  let interval = useRef<number>(0);
 
   const handleClick = () => {
-    dispatch(start());
+    if (started) {
+      window.clearInterval(interval.current);
+      dispatch(stop());
+    } else {
+      dispatch(start());
+      interval.current = window.setInterval(() => {
+        setCount(count => count + 1);
+      }, 1000);
+    }
   };
+
+  useEffect(() => {
+    return () => {
+      window.clearInterval(interval.current);
+    };
+  }, []);
+
+  let seconds = started
+    ? Math.floor((Date.now() - new Date(dateStart).getTime()) / 1000)
+    : 0;
+
+  const hours = seconds ? Math.floor(seconds / 60 / 60) : 0;
+  seconds -= hours * 60 * 60;
+
+  const minutes = seconds ? Math.floor(seconds / 60) : 0;
+  seconds -= minutes * 60;
 
   return (
     <div className={`${styles.recorder} ${started && styles.recorderStarted}`}>
       <button onClick={handleClick} className={styles.recorderRecord}>
         <span></span>
       </button>
-      <div className={styles.recorderCounter}>00:00:00</div>
+      <div className={styles.recorderCounter}>
+        {addZero(hours)}:{addZero(minutes)}:{addZero(seconds)}
+      </div>
     </div>
   );
 };
